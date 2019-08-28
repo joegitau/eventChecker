0cd21c33d91d103a8cab0922bccdefb1570621fd
Joe <Joe@Josephs-MacBook-Air.local>
2019-08-28 21:45:08 +0300

    created virtual relationship btwn users and events

--- models/User.js
+++ models/User.js
@@ -90,6 +90,9 @@ userSchema.virtual("events", {
   foreignField: "creator"
 });
 
+// delete user's events once profile is deleted
+userSchema.pre("remove", async function(next) {});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/events.js
+++ routes/events.js
@@ -5,11 +5,12 @@ Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
 const Event = require("../models/Event");
 const User = require("../models/User");
 
 // create event
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const schema = {
     title: Joi.string().required(),
     venue: Joi.string().required(),
@@ -18,20 +19,17 @@ router.post("/", async (req, res) => {
       .required(),
     eventTime: Joi.string().required(),
     duration: Joi.number().required(),
-    description: Joi.string(),
-    creator: Joi.objectId().required()
+    description: Joi.string()
   };
 
   const { error } = Joi.validate(req.body, schema);
   if (error) return res.status(400).send(error.details[0].message);
 
   try {
-    const event = new Event(req.body);
-
-    // const event = new Event({
-    //   ...req.body,
-    //   creator: req.user_id
-    // });
+    const event = new Event({
+      ...req.body,
+      creator: req.user._id
+    });
     await event.save();
     res.status(201).send(event);
   } catch (err) {
@@ -39,17 +37,28 @@ router.post("/", async (req, res) => {
   }
 });
 
-// fetch user's events
-router.get("/", async (req, res) => {
+// fetch all user's events
+router.get("/", auth, async (req, res) => {
   try {
-    const user = await User.findOne({ creator: req.body._id });
-
-    const events = await Event.find()
-      .populate("user")
-      .execPopulate();
+    // const events = await Event.find({ creator: req.user._id });
+    const events = req.user.ev;
     res.status(200).send(events);
   } catch (err) {
     res.status(400).send("Events not found");
+  }
+});
+
+// fetch single user event
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const event = await Event.findOne({
+      _id: req.params.id,
+      creator: req.user._id
+    });
+    if (!event) return res.status(400).send("Event with given ID not found");
+    res.status(200).send(event);
+  } catch (err) {
+    res.status(400).send("Event with given ID not found");
   }
 });
 
